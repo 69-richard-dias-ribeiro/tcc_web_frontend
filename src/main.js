2f05f356d5e7230a2ba6b33c8902575d2b947e7b
69-richard-dias-ribeiro <richardias9699@gmail.com>
2021-08-18 15:24:30 -0300

    renamed Home.vue to Login.vue and Main.vue to Home.vue

--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//import {library} from '@fortawesome/fontawesome-svg-core'
+//import {fas} from '@fortawesome/free-solid-svg-icons'
+//import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+
+//library.add(fas);
+//Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 createApp(App).use(store).use(router).mount('#app')
